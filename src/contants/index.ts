3571f2c345805ec6c53831604b796fb01e5b9598
Abdallah-Azme <abdallahazme199794@gmail.com>
2024-03-04 08:46:35 +0200

    init

---
+++ src/contants/index.ts
@@ -0,0 +1,19 @@
+import { BsBellFill, BsHouseFill } from "react-icons/bs";
+
+export const navItems = [
+  {
+    label: "Home",
+    href: "/",
+    icon: BsHouseFill,
+  },
+  {
+    label: "Notification",
+    href: "/notification",
+    icon: BsBellFill,
+  },
+  {
+    label: "Profile",
+    href: "/users/123",
+    icon: BsHouseFill,
+  },
+];
